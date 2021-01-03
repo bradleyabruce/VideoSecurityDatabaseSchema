1caf48056890c4a0c472ebfe3b041757ea5d2d63
Bradley Bruce <Bruceba@mail.uc.edu>
2021-01-03 14:51:02 -0500

    Rebuilt Tables after loss of MySQL Server
    - Modified field names in tCameras
    - Removed tCameraStates
    - Renamed tServer to tServers
    - Modified most fields in tServers
    - Added tServerStatus

--- VideoSecurity.sql
+++ VideoSecurity.sql
@@ -9,22 +9,26 @@ CREATE TABLE tUsers(
 
 CREATE TABLE tCameras(
     [CameraID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    [InternalIP] [varchar](16) NOT NULL,
-    [ExternalIP] [varchar](16) NOT NULL,
+    [InternalAddress] [varchar](16) NOT NULL,
+    [ExternalAddress] [varchar](16) NOT NULL,
     [CameraName] [varchar](30) NOT NULL,
     [MacAddress] [varchar](50) NOT NULL,
     [CameraLocationID] [int] NOT NULL,
     [CameraStatusID] [int] NOT NULL
 );
 
-CREATE TABLE tCameraStates
-(
-	[CameraStateID] [int] IDENTITY(1,1),
-	[CameraState] [varchar](50) NOT NULL,
+CREATE TABLE tServers(
+    [ServerID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [InternalAddress] [varchar](16) NOT NULL,
+    [ExternalAddress] [varchar](16) NOT NULL,
+    [MacAddress] [varchar](50) NOT NULL,
+	[Name] [varchar](20) NOT NULL,
+	[PortNumber] [int] NOT NULL,
+	[ServerStatusID] [int] NOT NULL,
+	[DirectoryPath] [varchar](100)
 );
 
-CREATE TABLE tCameraLocations
-(
-	[CameraLocationID] [int] IDENTITY(1,1),
-	[CameraLocation] [varchar](100) NOT NULL,
+CREATE TABLE tServerStatus(
+    [ServerStatusID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [ServerStatus] [varchar](25) NOT NULL
 );
